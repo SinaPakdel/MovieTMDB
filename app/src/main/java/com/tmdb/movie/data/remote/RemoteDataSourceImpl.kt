42f086eafe2987234c300f516002965b034c7648
sina <cena.pakdel@gmail.com>
2023-05-05 18:24:37 +0330

    fix package for RemoteDataSourceImpl

--- app/src/main/java/com/tmdb/movie/data/remote/RemoteDataSourceImpl.kt
+++ app/src/main/java/com/tmdb/movie/data/remote/RemoteDataSourceImpl.kt
@@ -1,4 +1,6 @@
 package com.tmdb.movie.data.remote
 
+import com.tmdb.movie.data.remote.service.MovieService
+
 class RemoteDataSourceImpl(private val movieService: MovieService) : RemoteDataSource{
 }
