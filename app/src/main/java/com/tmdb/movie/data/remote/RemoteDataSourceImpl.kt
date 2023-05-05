6e77a368d5652400685ffca046c538ce49bcdbe3
pooria-darvishi <pooria.darvishi.82@gmail.com>
2023-05-05 19:38:45 +0330

    refactor dataSource

--- app/src/main/java/com/tmdb/movie/data/remote/RemoteDataSourceImpl.kt
+++ app/src/main/java/com/tmdb/movie/data/remote/RemoteDataSourceImpl.kt
@@ -7,17 +7,17 @@ import com.tmdb.movie.util.mapper.asMovieDetailsItem
 import com.tmdb.movie.util.mapper.asMoviesItem
 
 class RemoteDataSourceImpl(private val movieService: MovieService) : RemoteDataSource {
-    suspend fun getPopularMovies(page: Int): List<MovieItem> {
+    override suspend fun getPopularMovies(page: Int): List<MovieItem> {
         val moviesDto = movieService.getPopularMovies(page).results
         return moviesDto.asMoviesItem()
     }
 
-    suspend fun getUpcomingMovies(page: Int): List<MovieItem> {
+    override suspend fun getUpcomingMovies(page: Int): List<MovieItem> {
         val moviesDto = movieService.getUpcomingMovies(page).results
         return moviesDto.asMoviesItem()
     }
 
-    suspend fun getMovieDetails(movieId: Int): MovieDetailsItem {
+    override suspend fun getMovieDetails(movieId: Int): MovieDetailsItem {
         val movieDetail = movieService.getMovieDetails(movieId)
         return movieDetail.asMovieDetailsItem()
     }
