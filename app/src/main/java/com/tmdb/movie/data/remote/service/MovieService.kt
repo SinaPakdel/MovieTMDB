e6304077af2a89e1074ef958da591f7048332ced
pooria-darvishi <pooria.darvishi.82@gmail.com>
2023-05-08 17:22:56 +0330

    Refactor MovieService with Response

--- app/src/main/java/com/tmdb/movie/data/remote/service/MovieService.kt
+++ app/src/main/java/com/tmdb/movie/data/remote/service/MovieService.kt
@@ -2,6 +2,7 @@ package com.tmdb.movie.data.remote.service
 
 import com.tmdb.movie.model.dto.details.MovieDetailsResponse
 import com.tmdb.movie.model.dto.movies.MovieResponse
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -9,11 +10,11 @@ import retrofit2.http.Query
 
 interface MovieService {
     @GET("movie/popular")
-    suspend fun getPopularMovies(@Query("page") page: Int): MovieResponse
+    suspend fun getPopularMovies(@Query("page") page: Int): Response<MovieService>
 
     @GET("movie/upcoming")
-    suspend fun getUpcomingMovies(@Query("page") page: Int): MovieResponse
+    suspend fun getUpcomingMovies(@Query("page") page: Int):  Response<MovieService>
 
     @GET("movie/{movie_id}")
-    suspend fun getMovieDetails(@Path("movie_id") movieId: Int): MovieDetailsResponse
+    suspend fun getMovieDetails(@Path("movie_id") movieId: Int): Response<MovieDetailsResponse>
 }
