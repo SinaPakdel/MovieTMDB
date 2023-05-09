4a8c7e7fa3fb068b1aad48931f6c92c855ea68fa
pooria-darvishi <pooria.darvishi.82@gmail.com>
2023-05-09 11:35:58 +0330

    refactor safe map to ResponseState

--- app/src/main/java/com/tmdb/movie/util/safe_api/ResponseState.kt
+++ app/src/main/java/com/tmdb/movie/util/safe_api/ResponseState.kt
@@ -1,7 +1,26 @@
 package com.tmdb.movie.util.safe_api
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+
 sealed class ResponseState<out T> {
-    object Loading :ResponseState<Nothing>()
+    object Loading : ResponseState<Nothing>()
     data class Error(val error: ErrorState) : ResponseState<Nothing>()
-    data class Success <out T>(val data : T) : ResponseState<T>()
+    data class Success<out T>(val data: T) : ResponseState<T>()
 }
+
+fun <T> Flow<T>.asResponseState(): Flow<ResponseState<T>> = map { response ->
+    ResponseState.Success(response) as ResponseState<T>
+}.onStart { emit(ResponseState.Loading) }
+    .catch { exception ->
+        when (exception) {
+            is UnSuccessfulResponseException -> {
+                emit(ResponseState.Error(ErrorState.ErrorCode(exception.message.toString())))
+            }
+            else -> {
+                emit(ResponseState.Error(ErrorState.ErrorMessage(exception.message.toString())))
+            }
+        }
+    }
